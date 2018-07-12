511ffea9fc5d9ea27d989a8cd10b0a639d19d1f8
Giorgio Pomettini <giorgio.pomettini@gmail.com>
2018-07-12 21:03:19 +0200

    Added pseudo-working implementation of leaderboard

--- Assets/CGJ2018/Scripts/LeaderboardManager.cs
+++ Assets/CGJ2018/Scripts/LeaderboardManager.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Newtonsoft.Json;
 
 public class LeaderboardManager : MonoBehaviour 
 {
 	private float startScore = 1000000;
 	private bool lowerScore = false;
+	private string filePath;
+
+	public ScreenshotManager ScreenshotManager;
 	public FloatVariable PlayerScore;
+	public List<PlayerScoreElement> PlayerScoreElements;
+
+	public void OnEnable()
+	{
+		filePath = Application.dataPath + "/Resources/Scores/Scores.txt";
+
+		LoadData();
+	}
 
 	public void StartRecording ()
 	{
@@ -23,5 +36,41 @@ public class LeaderboardManager : MonoBehaviour
 	{
 		if (lowerScore)
 			PlayerScore.Value -= Time.deltaTime;
+	}
+
+	public void AddPlayerScoreElement ()
+	{
+		Texture2D lastFace = ScreenshotManager.FaceScreenshots[ScreenshotManager.FaceScreenshots.Count - 1];
+		PlayerScoreElement element = new PlayerScoreElement(PlayerScore.Value, lastFace);
+		PlayerScoreElements.Add(element);
+
+		SaveData();
+	}
+
+	public void LoadData ()
+	{
+		TextAsset datafile = Resources.Load(this.name) as TextAsset;
+		
+		if (datafile)
+			PlayerScoreElements = JsonConvert.DeserializeObject<List<PlayerScoreElement>>(datafile.text);
+	}
+
+	public void SaveData ()
+	{
+		string dataContent = JsonConvert.SerializeObject(PlayerScoreElements);
+		System.IO.File.WriteAllText(filePath, dataContent);
+	}
+}
+
+[Serializable]
+public class PlayerScoreElement
+{
+	public float PlayerScore;
+	public Texture2D PlayerPhoto;
+
+	public PlayerScoreElement (float score, Texture2D photo)
+	{
+		this.PlayerScore = score;
+		this.PlayerPhoto = photo;
 	}
 }
