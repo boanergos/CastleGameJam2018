e16f7d8f301041812c97792c0ec660a0e7f34c7b
Giorgio Pomettini <giorgio.pomettini@gmail.com>
2018-07-12 18:36:36 +0200

    UI Tweaked a bit

--- Assets/CGJ2018/Scripts/MenuFacesScreen.cs
+++ Assets/CGJ2018/Scripts/MenuFacesScreen.cs
@@ -8,11 +8,24 @@ public class MenuFacesScreen : MonoBehaviour
 	public ScreenshotManager SM;
 
 	public Image[] FacePanels;
+	public Sprite BlackScreenshot;
 
 	void OnEnable () 
 	{
+		foreach (Image facePanel in FacePanels)
+		{
+			facePanel.sprite = BlackScreenshot;
+		}
+
+		StartCoroutine(ShowFaces());
+	}
+
+	IEnumerator ShowFaces ()
+	{
 		for (int i = 0; i < FacePanels.Length; i++)
 		{
+			yield return new WaitForSeconds(0.2f);
+
 			Texture2D tex2d = SM.FaceScreenshots[i];
 			Sprite spr = Sprite.Create(tex2d, new Rect(0, 0, tex2d.width, tex2d.height), Vector2.zero);
 
