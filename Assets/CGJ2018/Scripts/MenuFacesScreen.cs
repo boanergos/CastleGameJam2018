4b236a8ee77ec7ddf73d2071e6cd8be5f5828e2c
Giorgio Pomettini <giorgio.pomettini@gmail.com>
2018-07-12 16:48:55 +0200

    Added screenshot menu

---
+++ Assets/CGJ2018/Scripts/MenuFacesScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuFacesScreen : MonoBehaviour 
+{
+	public ScreenshotManager SM;
+
+	public Image[] FacePanels;
+
+	void OnEnable () 
+	{
+		for (int i = 0; i < FacePanels.Length; i++)
+		{
+			Texture2D tex2d = SM.FaceScreenshots[i];
+			Sprite spr = Sprite.Create(tex2d, new Rect(0, 0, tex2d.width, tex2d.height), Vector2.zero);
+
+			if (FacePanels[i])
+				FacePanels[i].sprite = spr;
+		}
+	}
+}
---
+++ Assets/CGJ2018/Scripts/ScreenshotManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotManager : MonoBehaviour 
+{
+	public List<Texture2D> FaceScreenshots;
+
+	private bool grab;
+
+	public void Init ()
+	{
+		FaceScreenshots = new List<Texture2D>();
+	}
+
+	// public void OnGUI ()
+	// {
+	// 	if (GUI.Button(new Rect(0, Screen.height / 2, Screen.width, Screen.height / 2), "SCREEN"))
+	// 	{
+	// 		Init();
+	// 		grab = true;
+	// 	}
+	// }
+
+	public void TakeScreenshot ()
+	{
+		grab = true;
+	}
+
+	private void OnPostRender()
+    {
+        if (grab)
+        {
+            Texture2D texture = new Texture2D(Screen.width / 2, Screen.height / 2, TextureFormat.RGB24, false);
+            texture.ReadPixels(new Rect(Screen.width / 4, 0, Screen.width / 2, Screen.height), 0, 0, false);
+            texture.Apply();
+
+			FaceScreenshots.Add(texture);
+
+            grab = false;
+        }
+    }
+}
